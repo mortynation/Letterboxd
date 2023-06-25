20ce8bd2dd462aa3a993c896ad43baa0262f6446
mortynation <nivaasanna24@gmail.com>
2023-06-24 23:21:50 -0400

    creating Movie entity

---
+++ movies-service/src/main/java/com/letterboxd/moviesservice/entity/Movie.java
@@ -0,0 +1,18 @@
+package com.letterboxd.moviesservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    private String movieName;
+    private String userName;
+}
